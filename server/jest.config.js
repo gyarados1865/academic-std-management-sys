1b6f980c927f92e2b8d8e354fc41a4042a7cc0d0
M Sibtain Khan <gyarados.1865@gmail.com>
2026-07-29 18:25:11 +0500

    Revert "git commit -m "test(auth): add integration tests and reusable test helpers""
    This reverts commit 0a072095c4969703b17e91d904fa6c40561ece3c.

--- server/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.js$": "babel-jest",
-  },
-  testMatch: ["**/tests/**/*.test.js"],
-  collectCoverage: false,
-};
--- server/tests/helpers/request.helper.js
+++
@@ -1,4 +0,0 @@
-import request from "supertest";
-import app from "../../src/app.js";
-
-export const apiRequest = request(app);
