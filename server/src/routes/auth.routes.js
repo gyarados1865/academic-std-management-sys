44e487adfacf9fd209c1b22b07d7c2a6223d385e
M Sibtain Khan <gyarados.1865@gmail.com>
2026-07-29 12:27:17 +0500

    feat(validation): add login request validation using Joi

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -4,11 +4,13 @@ import {
   authenticate,
   authorize,
 } from "../middleware/auth.middleware.js";
+import validate from "../validators/validation.middleware.js";
+import { loginSchema } from "../validators/auth.validator.js";
 
 const router = Router();
 
 router.post("/register", register);
-router.post("/login", login);
+router.post("/login", validate(loginSchema), login);
 router.get("/me", authenticate, (req, res) => {
   res.status(200).json({
     success: true,
